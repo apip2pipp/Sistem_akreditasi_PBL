ed59aaded7774f9704ea0b5eb01b9a00ce57c0d7
apip2pipp <afifkhosyidzaki@gmail.com>
2025-05-20 12:04:06 +0700

    .add user model

---
+++ app/Models/mUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class mUser extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'm_users';
+    protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'username',
+        'email',
+        'name',
+        'password',
+        'level_id'
+    ];
+
+    protected $hidden = ['password'];
+
+    public function getAuthIdentifierName()
+    {
+        return 'username'; // Agar Auth tahu login pakai kolom username
+    }
+
+
+}
