01a753f5365e4124034538b9bc8985133eb44e70
apip2pipp <afifkhosyidzaki@gmail.com>
2025-05-29 22:56:41 +0700

    .feat model pelaksanaan

---
+++ app/Models/tPelaksanaan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tPelaksanaan extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_pelaksanaans';
+    protected $primaryKey = 'id_pelaksanaan';
+    protected $fillable = ['pelaksanaan'];
+
+
+
+    public function akreditasi()
+    {
+        return $this->hasMany(tAkreditasi::class, 'penetapan_id', 'id_penetapan');
+    }
+}
