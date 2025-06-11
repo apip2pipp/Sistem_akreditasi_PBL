30270843719ddd8add2a9050e16287ab4a11b26a
= <=>
2025-06-11 19:33:17 +0700

    .refactor_new_function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\mDosen;
+use App\Models\mKriteria;
+use App\Models\mLevel;
+use App\Models\mUser;
+use App\Models\tAkreditasi;
+use App\Models\tPenelitianDosen;
+use App\Models\tPermissionKriteriaUser;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,13 +16,18 @@ class DashboardController extends Controller
     public function index()
     {
         $breadcrumb = (object) [
-            'title' => 'Welcome',
-            'list' => ['Home', 'Welcome']
+            'title' => 'Dashboard',
+            'list' => ['Home', 'Dashboard']
         ];
         $activeMenu = 'dashboard';
 
 
-        return view('dashboard.index', compact('breadcrumb', 'activeMenu', ));
+        $dataUser = mUser::count();
+        $dataLevel = mLevel::count();
+        $totalCriteria = mKriteria::count();
+        $totalPermission = tPermissionKriteriaUser::count();
+        $dataPenelitian = tPenelitianDosen::count();
+        $dataAkreditasikoordinator = tAkreditasi::count();
+        return view('dashboard.index', compact('breadcrumb', 'activeMenu','dataUser','dataLevel','totalCriteria','totalPermission','dataPenelitian'));
     }
 }
-
