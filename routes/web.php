31b21bdfa2e9f47372997b14396c2c1a6aded276
apip2pipp <afifkhosyidzaki@gmail.com>
2025-06-11 20:01:47 +0700

    .fix route

--- routes/web.php
+++ routes/web.php
@@ -88,39 +88,6 @@ Route::middleware(['auth'])->group(function () {
         Route::resource('user', MUserController::class);
         Route::post('user/list', [MUserController::class, 'list'])->name('user.list');
         Route::get('user/{id}/delete', [MUserController::class, 'confirm']);
-
-        //dosen
-        Route::get('dosen/import', [MDosenController::class, 'importForm'])->name('dosen.import');
-        Route::post('dosen/import', [MDosenController::class, 'import'])->name('dosen.import.post');
-        Route::get('dosen/export', [MDosenController::class, 'export'])->name('dosen.export');
-        Route::resource('dosen', MDosenController::class);
-        Route::post('dosen/list', [MDosenController::class, 'list'])->name('dosen.list');
-        Route::get('dosen/{id}/delete', [MDosenController::class, 'confirm']);
-
-        //kaprodi
-        Route::resource('kaprodi', MKaprodiController::class);
-        Route::post('kaprodi/list/data', [MKaprodiController::class, 'list'])->name('kaprodi.list');
-        Route::get('kaprodi/{id}/delete', [MKaprodiController::class, 'confirm']);
-
-        //MKoordinator
-        Route::resource('koordinator', MKoordinatorController::class);
-        Route::post('koordinator/list/data', [MKoordinatorController::class, 'list'])->name('koordinator.list');
-        Route::get('koordinator/{id}/delete', [MKoordinatorController::class, 'confirm']);
-
-        //MKJM
-        Route::resource('kjm', MKjmController::class);
-        Route::post('kjm/list/data', [MKjmController::class, 'list'])->name('kjm.list');
-        Route::get('kjm/{id}/delete', [MKjmController::class, 'confirm']);
-
-        //direktur-utama
-        Route::resource('direktur-utama', MDirutController::class);
-        Route::post('direktur-utama/list/data', [MDirutController::class, 'list'])->name('direktur-utama.list');
-        Route::get('direktur-utama/{id}/delete', [MDirutController::class, 'confirm']);
-
-        //kajur
-        Route::resource('ketua-jurusan', MKajurController::class);
-        Route::post('ketua-jurusan/list/data', [MKajurController::class, 'list'])->name('kajur.list');
-        Route::get('ketua-jurusan/{id}/delete', [MKajurController::class, 'confirm']);
     });
 
     Route::resource('kriteria', MKriteriaController::class);
