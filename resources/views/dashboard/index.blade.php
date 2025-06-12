7744ab268e032fd9a122f32ca8b2628054c3f6c2
apip2pipp <afifkhosyidzaki@gmail.com>
2025-06-12 17:55:00 +0700

    .refactor amiril dashboar to new info on dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,132 +7,197 @@
 
 
 
-<!-- ===========================================================-->
-                <!-- ADMIN DASHBOARD -->
-<!-- ===========================================================-->
- <!-- Data User -->
-@if (auth()->user()->level->level_kode === 'ADM')
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
+            <!-- ===========================================================-->
+            <!-- ADMIN DASHBOARD -->
+            <!-- ===========================================================-->
+            <!-- Data User -->
+            @if (auth()->user()->level->level_kode === 'ADM')
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
 
-                <div class="small-box bg-ijo6 text-ijo1">
-                    <div class="inner">
-                        <h3>{{ $dataUser }}</h3>
+                    <div class="small-box bg-ijo6 text-ijo1">
+                        <div class="inner">
+                            <h3>{{ $dataUser }}</h3>
 
-                        <p>Data User</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-                    {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
-                </div>
-            </div>
-
-
-            <!-- Data level -->
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-ijo1 text-ijo6">
-                    <div class="inner">
-                        <h3>{{ $dataLevel }}</h3>
-
-                        <p>Data Level</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
+                            <p>Data User</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        {{-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
                     </div>
                 </div>
-            </div>
 
-            <!-- Data Criteria -->
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-ijo6 text-ijo1">
-                    <div class="inner">
-                        <h3>{{ $totalCriteria }}</h3>
 
-                        <p>Data Criteria</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
-                    </div>
-                </div>
-            </div>
+                <!-- Data level -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo1 text-ijo6">
+                        <div class="inner">
+                            <h3>{{ $dataLevel }}</h3>
 
-            <!-- Data Permission -->
-            <div class="col-lg-3 col-6">
-               <div class="small-box bg-ijo1 text-ijo6">
-                    <div class="inner">
-                        <h3>{{ $totalPermission }}</h3>
-
-                        <p>Data Permission</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
+                            <p>Data Level</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
-@endif
 
-<!-- ===========================================================-->
-                <!-- KOORDINATOR DASHBOARD -->
-<!-- ===========================================================-->
-@if (auth()->user()->level->level_kode === 'KDR')
-            <!-- Data Penelitian -->
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-ijo6 text-ijo1">
-                    <div class="inner">
-                        <h3>{{ $dataPenelitian }}</h3>
-                        <p>Data Penelitian</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
+                <!-- Data Criteria -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo6 text-ijo1">
+                        <div class="inner">
+                            <h3>{{ $totalCriteria }}</h3>
+
+                            <p>Data Criteria</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+                <!-- Data Permission -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo1 text-ijo6">
+                        <div class="inner">
+                            <h3>{{ $totalPermission }}</h3>
 
-            <!-- Total Kriteria -->
-            <div class="col-lg-3 col-6">
-               <div class="small-box bg-ijo1 text-ijo6">
-                    <div class="inner">
-                        <h3>{{ $totalPermission }}</h3>
-                        <p>Total Criteria</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-bag"></i>
+                            <p>Data Permission</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
-@endif
 
-<!-- ===========================================================-->
-                <!-- DOSEN DASHBOARD -->
-<!-- ===========================================================-->
+                <div class="card-body">
+    <div class="p-4 rounded shadow-sm" style="background: #e6ffe6; border-left: 5px solid #2e7d32;">
+        <h4 class="mb-3" style="font-weight: bold; color: #1b5e20;">
+            <i class="fas fa-certificate"></i> Welcome, {{ auth()->user()->name }}!
+        </h4>
+        <p>
+            This platform is designed to support the accreditation process. Accreditation is a critical mechanism used to evaluate and ensure the quality of educational institutions and programs.
+        </p>
+        <p>
+            Through this system, we aim to collect, organize, and present essential data that reflect academic performance, institutional development, and continuous improvement efforts.
+        </p>
+        <p>
+            Your participation is vital. By providing accurate and timely information, you contribute to enhancing transparency, accountability, and the overall quality assurance of our institution.
+        </p>
+        <hr>
 
-
-
-<!-- ===========================================================-->
-                <!-- DOSEN KPS -->
-<!-- ===========================================================-->
-
-
-
-<!-- ===========================================================-->
-                <!-- DOSEN KAJUR -->
-<!-- ===========================================================-->
-
-
-
-<!-- ===========================================================-->
-                <!-- DOSEN KJM -->
-<!-- ===========================================================-->
+    </div>
+</div>
 
 
+            @endif
 
-<!-- ===========================================================-->
-                <!-- DOSEN DIRUT -->
-<!-- ===========================================================-->
+            <!-- ===========================================================-->
+            <!-- KOORDINATOR DASHBOARD -->
+            <!-- ===========================================================-->
+            @if (auth()->user()->level->level_kode === 'KDR')
+                <!-- Data Penelitian -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo6 text-ijo1">
+                        <div class="inner">
+                            <h3>{{ $dataPenelitianKoordinator }}</h3>
+                            <p>Data Penelitian</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                    </div>
+                </div>
 
-{{--
+
+                <!-- Total Kriteria -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo1 text-ijo6">
+                        <div class="inner">
+                            <h3>{{ $totalCriteria }}</h3>
+                            <p>Total Criteria</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+            <!-- ===========================================================-->
+            <!-- DOSEN DASHBOARD -->
+            <!-- ===========================================================-->
+            @if (auth()->user()->level->level_kode === 'DSN')
+                <!-- Data Penelitian -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo6 text-ijo1">
+                        <div class="inner">
+                            <h3>{{ $dataPenelitianDosen }}</h3>
+                            <p>Data Penelitian</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+
+
+            <!-- ===========================================================-->
+            <!-- KPS -->
+            <!-- ===========================================================-->
+            <!-- ===========================================================-->
+            <!-- KPS DASHBOARD -->
+            <!-- ===========================================================-->
+            @if (in_array(auth()->user()->level->level_kode, ['KPD', 'DIR', 'KJM', 'KJR']))
+                <!-- Total Dosen -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo6 text-ijo1">
+                        <div class="inner">
+                            <h3>{{ $totalDosen }}</h3>
+                            <p>Total Team Akreditasi</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Total Akreditasi -->
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-ijo1 text-ijo6">
+                        <div class="inner">
+                            <h3>{{ $totalAkreditasi }}</h3>
+                            <p>Total Akreditasi</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-university"></i>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+
+
+            <!-- ===========================================================-->
+            <!-- KAJUR -->
+            <!-- ===========================================================-->
+
+
+
+            <!-- ===========================================================-->
+            <!-- KJM -->
+            <!-- ===========================================================-->
+
+
+
+            <!-- ===========================================================-->
+            <!-- DIRUT -->
+            <!-- ===========================================================-->
+
+            {{--
             <!-- ./col -->
             <div class="col-lg-3 col-6">
                 <!-- small box -->
